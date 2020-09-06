114d1bbe89c6d6bda18bf5b94dd2745106e487ba
Amr Saber <amr.m.saber.mail@gmail.com>
2020-09-06 17:00:45 +0200

    :heavy_check_mark: Add tests

---
+++ unit.test.js
@@ -0,0 +1,102 @@
+const delay = require('delay');
+const faker = require('faker');
+const Redis = require('ioredis');
+
+const { lock } = require('.');
+
+describe('Lock tests', () => {
+  /**
+   * @type {import('ioredis').Redis}
+   */
+  let redis;
+
+  beforeAll(() => {
+    redis = new Redis();
+  });
+
+  afterEach(async () => {
+    await redis.flushdb();
+  });
+
+  describe('Locking functionality', () => {
+    let counter = 0;
+
+    async function unsafeIncrement() {
+      await delay(faker.random.number({ min: 10, max: 50 }));
+      const counterValue = counter;
+
+      await delay(faker.random.number({ min: 10, max: 50 }));
+      counter = counterValue + 1;
+    }
+
+    test('The unsafe function has race condition', async () => {
+      counter = 0;
+      await Promise.all(new Array(20).fill(0).map(unsafeIncrement));
+      expect(counter).not.toEqual(20);
+    });
+
+    test('Locks handle race conditions', async () => {
+      counter = 0;
+
+      await Promise.all(new Array(20).fill(0).map(async () => {
+        const unlock = await lock(redis, 'test');
+        await unsafeIncrement();
+        await unlock();
+      }));
+
+      expect(counter).toEqual(20);
+    });
+  });
+
+  describe('options', () => {
+    test('timeout', async () => {
+      const lockName = 'timeout-test';
+
+      const timeBeforeLock = new Date().valueOf();
+      await lock(redis, lockName, { timeoutMillis: 200 });
+      const unlock = await lock(redis, lockName);
+      const timeAfterLock = new Date().valueOf();
+
+      await unlock();
+
+      expect(timeAfterLock - timeBeforeLock).toBeGreaterThanOrEqual(200);
+      expect(timeAfterLock - timeBeforeLock).toBeLessThanOrEqual(220);
+    });
+
+    test('retry time', async () => {
+      const lockName = 'retry-test';
+
+      await lock(redis, lockName, { timeoutMillis: 20 });
+
+      const timeBeforeLock = new Date().valueOf();
+      const unlock = await lock(redis, lockName, { retryTimeMillis: 200 });
+      const timeAfterLock = new Date().valueOf();
+
+      await unlock();
+
+      expect(timeAfterLock - timeBeforeLock).toBeGreaterThanOrEqual(200);
+      expect(timeAfterLock - timeBeforeLock).toBeLessThanOrEqual(220);
+    });
+
+    test('fail time', async () => {
+      const lockName = 'fail-test';
+
+      const unlock = await lock(redis, lockName);
+
+      const timeBeforeLock = new Date().valueOf();
+      let timeAfterLock;
+
+      try {
+        await lock(redis, lockName, { failAfterMillis: 200 });
+        fail('Lock did not fail'); // eslint-disable-line no-undef
+      } catch (err) {
+        timeAfterLock = new Date().valueOf();
+      }
+
+      await unlock();
+
+      expect(timeAfterLock - timeBeforeLock).toBeGreaterThanOrEqual(200);
+      expect(timeAfterLock - timeBeforeLock).toBeLessThanOrEqual(220);
+    });
+  });
+});
