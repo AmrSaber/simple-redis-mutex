8f4d91dc529c48221f5a5dc5bd8312b8213bd720
Amr Saber <amr.m.saber.mail@gmail.com>
2022-12-02 15:33:29 +0200

    add tests for fifo lock

--- unit.test.js
+++ unit.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 const delay = require('delay');
 const faker = require('faker');
 const Redis = require('ioredis');
@@ -102,6 +103,63 @@ describe('Lock tests', () => {
       // Assert that the lock function is no longer attempting to acquire the lock asynchronously
       const lockValue = await redis.get('@simple-redis-mutex:lock-fail-test');
       expect(lockValue).toBeNull();
+    });
+  });
+
+  describe('fifo', () => {
+    let elements = [];
+
+    async function unsafeAppend(withLock = false) {
+      const push = async index => {
+        await delay(faker.datatype.number({ min: 10, max: 50 }));
+        elements.push(index);
+      };
+
+      const lockName = 'push-lock';
+
+      const promises = [];
+
+      for (let i = 0; i < 20; i++) {
+        await delay(5);
+
+        let pushPromise = Promise.resolve();
+        if (withLock) {
+          // eslint-disable-next-line no-loop-func
+          pushPromise = new Promise(resolve => {
+            lock(redis, lockName, { fifo: true })
+              .then(unlock => push(i).then(() => unlock()))
+              .then(resolve);
+          });
+        } else {
+          pushPromise = push(i);
+        }
+
+        promises.push(pushPromise);
+      }
+
+      await Promise.all(promises);
+    }
+
+    function isConsecutive(arr) {
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] > arr[i]) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    test('unsafeAppend is unsafe', async () => {
+      elements = [];
+      await unsafeAppend();
+      expect(isConsecutive(elements)).toBe(false);
+    });
+
+    test('FIFO lock works', async () => {
+      elements = [];
+      await unsafeAppend(true);
+      expect(isConsecutive(elements)).toBe(true);
     });
   });
 });
