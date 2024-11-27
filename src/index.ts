7ef1c9a97489c3448dcefe8e626f9a7e95736d4f
Amr Saber <amr.m.saber.mail@gmail.com>
2024-11-27 01:43:26 +0000

    add lock function

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,102 @@
 import { RedisClientType, RedisClusterType } from 'redis';
-import { releaseScript } from './lua';
 
 type RedisClient = RedisClientType<any, any, any> | RedisClusterType<any, any, any>;
 
-type ReleaseCallbackFn = () => unknown;
+type ReleaseCallbackFn = () => void;
 type ReleaseCallback = { lockKey: string; callback: ReleaseCallbackFn };
 
 export type ReleaseFunc = () => Promise<void>;
-export type TryLockParams = {
-  timeout?: number;
+export type TryLockOptions = { timeout?: number };
+export type LockOptions = TryLockOptions & {
+  pollingInterval?: number;
+  failAfter?: number;
+  onFail?: () => void;
 };
 
-const REDIS_UPDATE_CHANNEL = '@simple-redis-mutex:locks-releases';
+const REDIS_RELEASES_CHANNEL = '@simple-redis-mutex:locks-releases';
 const REDIS_OK = 'OK';
+
+export const DEFAULT_TIMEOUT = 30_000;
+export const DEFAULT_POLLING_INTERVAL = 10_000;
+
+let callbacks: ReleaseCallback[] = [];
+
+/**
+ * Attempts to acquire lock, if lock is already acquired it will block until it can acquire the lock.
+ * Returns lock release function.
+ *
+ * Lock timeout is used to expire the lock if it's not been released before `timeout`.
+ * This is to prevent crashed processes holding the lock indefinitely.
+ *
+ * When a lock is released redis Pub/Sub is used to publish that the lock has been released
+ * so that other processes waiting for the lock can attempt to acquire it.
+ *
+ * Manual polling is also implemented to attempt to acquire the lock in case the holder crashed and did not release the lock.
+ * It is controlled by `pollingInterval`.
+ *
+ * Application logic should not depend on lock timeout and polling interval. They are meant to be a safe net when things fail.
+ * Depending on them is inefficient and an anti-pattern, in such case application logic should be revised and refactored.
+ *
+ * If process fails to acquire the lock before `failAfter` milliseconds, it will throw an error and call `onFail` if provided.
+ * If `failAfter` is not provided, process will block indefinitely waiting for the lock to be released.
+ *
+ * @param redis redis client
+ * @param lockName lock name
+ * @param options lock options
+ * @param options.timeout lock timeout in milliseconds, default: 30 seconds
+ * @param options.pollingInterval how long between manual polling for lock status milliseconds, default: 10 seconds
+ * @param options.failAfter time to fail after if lock is still not acquired milliseconds
+ * @param options.onFail called when failed to acquire lock before `failAfter`
+ * @returns release function
+ */
+export function lock(
+  redis: RedisClient,
+  lockName: string,
+  { timeout = DEFAULT_TIMEOUT, pollingInterval = DEFAULT_POLLING_INTERVAL, failAfter, onFail }: LockOptions = {},
+): Promise<ReleaseFunc> {
+  return new Promise((resolve, reject) => {
+    let pollingId: NodeJS.Timeout | undefined;
+    let failId: NodeJS.Timeout | undefined;
+
+    let attempting = true;
+    function attempt() {
+      if (!attempting) return;
+
+      tryLock(redis, lockName, { timeout }).then(([hasLock, release]) => {
+        if (!hasLock) return;
+
+        clean();
+        resolve(release);
+      });
+    }
+
+    function clean() {
+      attempting = false;
+
+      // Remove release callback
+      callbacks = callbacks.filter((cb) => cb.callback != attempt);
+
+      // Clear timeouts
+      if (pollingId != null) clearInterval(pollingId);
+      if (failId != null) clearTimeout(failId);
+
+      pollingId = failId = undefined;
+    }
+
+    callbacks.push({ lockKey: getLockKey(lockName), callback: attempt });
+    if (pollingInterval != null) pollingId = setInterval(attempt, pollingInterval);
+
+    if (failAfter != null) {
+      failId = setTimeout(() => {
+        clean();
+        onFail?.();
+        reject(new Error(`Lock "${lockName}" could not be acquired after ${failAfter} millis`));
+      }, failAfter);
+    }
+
+    attempt();
+  });
+}
 
 /**
  * Try to acquire the lock, if failed will return immediately.
@@ -24,17 +108,17 @@ const REDIS_OK = 'OK';
  * If lock was not acquired, release function is a no-op.
  *
  * @param redis redis client
- * @param lockName Lock name
- * @param options Lock options
- * @param options.timeout Lock timeout in milliseconds, default: 30 seconds
- * @returns
+ * @param lockName lock name
+ * @param options lock options
+ * @param options.timeout lock timeout in milliseconds, default: 30 seconds
+ * @returns whether or not the lock was acquired and release function.
  */
 export async function tryLock(
   redis: RedisClient,
   lockName: string,
-  { timeout = 30_000 }: TryLockParams = {},
+  { timeout = DEFAULT_TIMEOUT }: TryLockOptions = {},
 ): Promise<[boolean, ReleaseFunc]> {
-  const lockKey = `@simple-redis-mutex:lock-${lockName}`;
+  const lockKey = getLockKey(lockName);
   const lockValue = String(Math.random());
 
   await listenForUpdates(redis);
@@ -53,7 +137,7 @@ export async function tryLock(
 
     await redis
       .eval(releaseScript, {
-        keys: [lockKey, REDIS_UPDATE_CHANNEL],
+        keys: [lockKey, REDIS_RELEASES_CHANNEL],
         arguments: [lockValue],
       })
       .catch((err) => console.error(`Error releasing lock ${lockName}:`, err));
@@ -62,38 +146,53 @@ export async function tryLock(
   return [true, release];
 }
 
-let listening = false;
+let subscriber: RedisClient | undefined;
 async function listenForUpdates(redis: RedisClient) {
-  if (listening) return;
-  listening = true;
+  // Make sure only one subscriber is created
+  if (subscriber != null && subscriber.isOpen) return;
 
-  const subscriber = redis.duplicate();
+  subscriber = redis.duplicate();
   subscriber.on('error', (err) => console.error('simple-redis-mutex subscriber error:', err));
   await subscriber.connect();
 
-  await subscriber.subscribe(REDIS_UPDATE_CHANNEL, async (message: string) => {
+  await subscriber.subscribe(REDIS_RELEASES_CHANNEL, async (message: string) => {
     const releasedLock: { key: string; value: string } = JSON.parse(message);
 
-    // Find related callback functions and remove them from callbacks
+    // Find related callback functions
     const relatedCallbacks = callbacks.filter((cb) => cb.lockKey == releasedLock.key);
-    callbacks = callbacks.filter((cb) => cb.lockKey != releasedLock.key);
 
     // Run all callbacks
     await Promise.all(relatedCallbacks.map((cb) => Promise.resolve(cb.callback())));
   });
 
   redis.on('end', async () => {
-    await subscriber.unsubscribe(REDIS_UPDATE_CHANNEL);
-    await subscriber.quit();
+    await subscriber?.unsubscribe(REDIS_RELEASES_CHANNEL);
+    await subscriber?.quit();
+    subscriber = undefined;
   });
 }
 
-let callbacks: ReleaseCallback[] = [];
-function addReleaseCallback(lockKey: string, callbackFn: ReleaseCallbackFn) {
-  callbacks.push({ lockKey, callback: callbackFn });
+function getLockKey(lockName: string): string {
+  return `@simple-redis-mutex:lock-${lockName}`;
 }
 
-// FIXME: is this needed?
-function removeReleaseCallback(lockKey: string, callbackFn: ReleaseCallbackFn) {
-  callbacks = callbacks.filter((cb) => !(cb.lockKey == lockKey && cb.callback == callbackFn));
-}
+/**
+ * Release the lock only if it has the same lockValue as acquireLock sets it.
+ * This will prevent the release of an already released lock.
+ *
+ * Script source: https://redis.io/commands/set#patterns -- Redis official docs + with small changes
+ */
+const releaseScript = `
+  local lockKey = KEYS[1]
+  local updatesChannel = KEYS[2]
+  local lockValue = ARGV[1]
+
+  if redis.call("GET", lockKey) == lockValue then
+      redis.call("DEL", lockKey)
+      redis.call(
+        "PUBLISH",
+        updatesChannel,
+        string.format('{ "key": "%s", "value": "%s" }', lockKey, lockValue)
+      )
+  end
+`;
--- unit.test.ts
+++ unit.test.ts
@@ -1,10 +1,15 @@
-import delay from 'delay';
 import { createClient, RedisClientType } from 'redis';
-import { tryLock } from './src';
+import { lock, LockOptions, tryLock, TryLockOptions } from './src';
 
 describe('Lock tests', () => {
   let redis: RedisClientType<any, any, any>;
   const lockName = '_test_lock';
+
+  function sleep(millis?: number): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(resolve, millis);
+    });
+  }
 
   beforeAll(async () => {
     redis = await createClient()
@@ -22,13 +27,11 @@ describe('Lock tests', () => {
 
   describe('tryLock', () => {
     test('it acquires the lock successfully', async () => {
-      let [hasLock, release] = await tryLock(redis, lockName);
+      let [hasLock] = await tryLock(redis, lockName);
       expect(hasLock).toEqual(true);
 
       [hasLock] = await tryLock(redis, lockName);
       expect(hasLock).toEqual(false);
-
-      await release();
     });
 
     test('no race conditions', async () => {
@@ -36,53 +39,148 @@ describe('Lock tests', () => {
       async function safeIncrement() {
         const [hasLock, release] = await tryLock(redis, lockName);
         if (!hasLock) return;
-        await delay(50);
+
+        await sleep(50);
         count = count + 1;
+
         await release();
       }
 
-      await Promise.all(new Array(100).fill(null).map(safeIncrement));
+      await Promise.all(new Array(20).fill(0).map(safeIncrement));
       expect(count).toEqual(1);
     });
 
     test('lock expiration', async () => {
-      const [hasLock] = await tryLock(redis, lockName, { timeout: 50 });
+      let [hasLock] = await tryLock(redis, lockName, { timeout: 50 });
       expect(hasLock).toEqual(true);
-
-      const [secondCall] = await tryLock(redis, lockName);
-      expect(secondCall).toEqual(false);
-
-      await delay(100);
-
-      const [afterTimeout] = await tryLock(redis, lockName);
-      expect(afterTimeout).toEqual(true);
-    });
-
-    test('expired owner cannot release the lock', async () => {
-      let [hasLock, expiredRelease] = await tryLock(redis, lockName, {
-        timeout: 50,
-      });
-      expect(hasLock).toEqual(true);
-
-      let [failedLock, failedRelease] = await tryLock(redis, lockName, { timeout: 50 });
-      expect(failedLock).toEqual(false);
-      await failedRelease();
-
-      await delay(100);
-
-      let [afterTimeout, release] = await tryLock(redis, lockName, { timeout: 50 });
-      expect(afterTimeout).toEqual(true);
-
-      await expiredRelease();
 
       [hasLock] = await tryLock(redis, lockName);
       expect(hasLock).toEqual(false);
 
+      await sleep(55);
+
+      [hasLock] = await tryLock(redis, lockName);
+      expect(hasLock).toEqual(true);
+    });
+
+    test('expired owner cannot release the lock', async () => {
+      const options: TryLockOptions = { timeout: 50 };
+
+      let [hasLock, expiredRelease] = await tryLock(redis, lockName, options);
+      expect(hasLock).toEqual(true);
+
+      let [failedLock, failedRelease] = await tryLock(redis, lockName, options);
+      expect(failedLock).toEqual(false);
+      await failedRelease();
+
+      await sleep(55);
+
+      let [afterTimeout, release] = await tryLock(redis, lockName, options);
+      expect(afterTimeout).toEqual(true);
+
+      await expiredRelease();
+
+      [hasLock] = await tryLock(redis, lockName, options);
+      expect(hasLock).toEqual(false);
+
       await release();
 
-      [hasLock, release] = await tryLock(redis, lockName, { timeout: 50 });
+      [hasLock, release] = await tryLock(redis, lockName, options);
       expect(hasLock).toEqual(true);
       await release();
+    });
+  });
+
+  describe('lock', () => {
+    describe('locking functionality', () => {
+      let counter = 0;
+      const increments = 20;
+
+      async function unsafeIncrement() {
+        await sleep();
+        const counterValue = counter;
+
+        await sleep();
+        counter = counterValue + 1;
+      }
+
+      test('unsafe function has race condition', async () => {
+        counter = 0;
+        await Promise.all(new Array(increments).fill(0).map(unsafeIncrement));
+        expect(counter).not.toEqual(increments);
+      });
+
+      test('locks solve race conditions', async () => {
+        counter = 0;
+
+        await Promise.all(
+          new Array(increments).fill(0).map(async () => {
+            const release = await lock(redis, lockName);
+            await unsafeIncrement();
+            await release();
+          }),
+        );
+
+        expect(counter).toEqual(increments);
+      });
+
+      test('lock is acquired as soon as it is released', async () => {
+        const options: LockOptions = { pollingInterval: 60_000 };
+
+        let release = await lock(redis, lockName, options);
+
+        const startTime = Date.now();
+        [, release] = await Promise.all([release(), lock(redis, lockName, options)]);
+
+        const timeTaken = Date.now() - startTime;
+        expect(timeTaken).toBeLessThan(5);
+
+        const [hasLock] = await tryLock(redis, lockName);
+        expect(hasLock).toEqual(false);
+
+        await release();
+      });
+    });
+
+    describe('options', () => {
+      test('timeout and polling', async () => {
+        const startTime = Date.now();
+        await lock(redis, lockName, { timeout: 50 });
+        const release = await lock(redis, lockName, { pollingInterval: 25 });
+        const timeTaken = Date.now() - startTime;
+
+        await release();
+
+        expect(timeTaken).toBeGreaterThanOrEqual(50);
+        expect(timeTaken).toBeLessThanOrEqual(100);
+      });
+
+      test('failAfter and onFail', async () => {
+        const release = await lock(redis, lockName);
+
+        const onFail = jest.fn();
+        const startTime = Date.now();
+        let timeTaken;
+
+        try {
+          await lock(redis, lockName, { failAfter: 50, pollingInterval: 25, onFail });
+          fail('lock did not fail'); // eslint-disable-line no-undef
+        } catch (err) {
+          expect((err as Error).message).toContain(`"${lockName}"`);
+          timeTaken = Date.now() - startTime;
+        }
+
+        expect(timeTaken).toBeGreaterThanOrEqual(50);
+        expect(timeTaken).toBeLessThanOrEqual(100);
+        expect(onFail).toBeCalledTimes(1);
+
+        await release();
+        await sleep(500);
+
+        // Assert that the lock function is no longer attempting to acquire the lock asynchronously
+        const [hasLock] = await tryLock(redis, lockName);
+        expect(hasLock).toEqual(true);
+      });
     });
   });
 });
